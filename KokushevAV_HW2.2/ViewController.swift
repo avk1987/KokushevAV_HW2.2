2010723f844b8a889875367807447da395273b72
AVK <kokushev@yandex.ru>
2020-02-28 00:26:49 +0500

    реализована логика

--- KokushevAV_HW2.2/ViewController.swift
+++ KokushevAV_HW2.2/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var colorView: UIView!
+    
+    // viewdidload не нужна, убрал
+    
+    @IBAction func sliderMoved(_ sender: UISlider, forEvent event: UIEvent) { // убрать uievent
+        changeColorAndValue(colorTag: sender.tag, and: sender.value)
     }
-
+    
+    
+    
 
 }
 
+extension ViewController {
+
+    func changeColorAndValue(colorTag: Int, and value: Float) {
+      
+        switch colorTag {
+        case 0:
+            redValue.text = String(format: "%.2f", value)
+        case 1:
+            greenValue.text = String(format: "%.2f", value)
+        case 2:
+            blueValue.text = String(format: "%.2f", value)
+        default:
+            break
+        }
+        
+        // не нашел способ исправить только один канал цвета, пришлось брать все цвета из лейбла и применять их к вьюхе.
+        
+        let redColor = CGFloat(Double(redValue.text ?? "0") ?? 0)
+        let greenColor = CGFloat(Double(greenValue.text ?? "0") ?? 0)
+        let blueColor = CGFloat(Double(blueValue.text ?? "0") ?? 0)
+        let alphaDefault = CGFloat(1.0)
+
+        colorView.backgroundColor = UIColor(red: redColor,
+                                            green: greenColor,
+                                            blue: blueColor,
+                                            alpha: alphaDefault)
+        
+
+        
+
+    }
+    
+}
