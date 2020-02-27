a600ba04905a57270b76264f547f0f6841989231
AVK <kokushev@yandex.ru>
2020-02-27 19:00:12 +0500

    Initial Commit

---
+++ KokushevAV_HW2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KokushevAV_HW2.2
+//
+//  Created by Александр Кокушев on 27.02.2020.
+//  Copyright © 2020 Александр Кокушев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
